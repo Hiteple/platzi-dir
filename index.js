666df8c1ef72ef2be9f0ead8860d97aa345b3392
hiteple <hiteplegold@gmail.com>
2020-01-04 17:08:30 -0300

    First commit: scripts for dev-env, lint, lint-fix, and graphQL integration with express

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define Schema
+const schema = buildSchema(`
+   type Query {
+      hello: String
+   }`)
+
+// Configure resolvers
+const resolvers = {
+  hello: () => 'Hello world!'
+}
+
+// Use GraphQL middleware with express (express-graphql)
+// schema goes in schema, resolvers goes in rootValue, and graphiql is the development environment
+app.use(
+  '/api',
+  gqlMiddleware({
+    schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+)
+
+app.listen(port, () =>
+  console.log(`Server listening at http://localhost:${port}`)
+)
